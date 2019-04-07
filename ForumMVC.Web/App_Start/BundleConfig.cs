76168869ce95aa292fc2cc9fea2e221b3dcdfd77
Simakowich Andrew <simagomel@gmail.com>
2019-04-07 22:14:12 +0300

    0704 bootstrap

--- ForumMVC.Web/App_Start/BundleConfig.cs
+++ ForumMVC.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ForumMVC.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.css","~/Content/bootstrap/bootstrap.scss",
                       "~/Content/site.css"));
         }
     }
